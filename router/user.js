ba42420105f55e7ba128c51f66fbd11115df96e2
abdumannon <89688510+abdumanno@users.noreply.github.com>
2022-10-31 17:53:30 +0500

    mern status

--- router/user.js
+++ router/user.js
@@ -4,24 +4,28 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const users = await User.find();
+   res.status(200)
   await res.send(users)
 });
 
 router.post("/", async (req, res) => {
   const { name, age } = req.body;
   await User.create({ name, age });
+   res.status(201)
   res.send("Malumot saqlandi!!")
 });
 
 router.delete("/:id", async (req, res) => {
   const user = req.params.id
   await User.findByIdAndDelete(user);
+   res.status(202)
   res.send("Ma'lumot o'chirildi")
 })
 
 router.put("/", async (req, res) => {
   const { name, age, id } = req.body;
   await User.findByIdAndUpdate(id, { name, age })
+   res.status(201)
   console.log("o'zgartirildi");
 });
 
